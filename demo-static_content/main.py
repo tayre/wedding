c326b94a669b8bdfdd1c926987c2d4197dfd1999
Tom Ayre <tayre@blackberry.com>
2014-01-03 12:54:30 -0500

    cleanup

--- demo-static_content/main.py
+++ demo-static_content/main.py
@@ -18,25 +18,26 @@ import os
 import jinja2
 import webapp2
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/login.html')
+        self.response.write(template.render())
+
+    def post(self):
         if self.request.get('pass') == 'password':
-            self.response.set_cookie('mykey', 'myvalue', max_age=3600, path='/')
-            template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-            self.response.write(template.render())
+            self.response.set_cookie()
+            return webapp2.redirect('/', code=303, response = self.response) #Use PRG to avoid duplicate form submission
 
         else:
-            template = JINJA_ENVIRONMENT.get_template('templates/login.html')
-            self.response.write(template.render())
+            return webapp2.redirect('/login') #PRG
 
 class IndexHandler(webapp2.RequestHandler):
     def get(self):
-        if self.request.cookies.get('mykey') == 'myvalue': 
+        if self.request.cookies.get('TNTSESSION') != None: 
             template = JINJA_ENVIRONMENT.get_template('templates/index.html')
             self.response.write(template.render())
 
@@ -44,38 +45,20 @@ class IndexHandler(webapp2.RequestHandler):
             template = JINJA_ENVIRONMENT.get_template('templates/login.html')
             self.response.write(template.render())
 
-
 class RSVPHandler(webapp2.RequestHandler):
     def get(self):
-        if self.request.cookies.get('mykey') == 'myvalue': 
+        if self.request.cookies.get('TNTSESSION') != None: 
             template = JINJA_ENVIRONMENT.get_template('templates/rsvp.html')
             self.response.write(template.render())
-
         else:
             template = JINJA_ENVIRONMENT.get_template('templates/login.html')
             self.response.write(template.render())
 
-class HotelHandler(webapp2.RequestHandler):
-    def get(self):
-        template_values = {
-            'data': 'lorem ipsum',
-            'more_data': 'bar',
-        }
-        template = JINJA_ENVIRONMENT.get_template('templates/hotel.html')
-        self.response.write(template.render(template_values))
 
-class RegistryHandler(webapp2.RequestHandler):
-    def get(self):
-            template = JINJA_ENVIRONMENT.get_template('templates/registry.html')
-            self.response.write(template.render())
-
-            
 routes = [
     ('/', IndexHandler),
-    ('/rsvp', RSVPHandler),
-    ('/hotel', HotelHandler),
-    ('/registry', RegistryHandler),
     ('/login', LoginHandler),
+    ('/rsvp', RSVPHandler),
 ]
 
 app = webapp2.WSGIApplication(routes, debug=True)
@@ -85,4 +68,10 @@ def handle_404(request, response, exception):
     response.set_status(404)
     response.write(template.render())
 
+def handle_500(request, response, exception):
+    template = JINJA_ENVIRONMENT.get_template('templates/500.html')
+    response.set_status(404)
+    response.write(template.render())
+
 app.error_handlers[404] = handle_404
+app.error_handlers[500] = handle_500
